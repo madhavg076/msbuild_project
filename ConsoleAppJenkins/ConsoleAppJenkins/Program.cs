8139db8a9198d3f4141765533cb2d299a7518e5e
Madhava G <72064079+madhavg076@users.noreply.github.com>
2022-07-13 23:01:06 +0530

    Files added

---
+++ ConsoleAppJenkins/ConsoleAppJenkins/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleAppJenkins
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello,World!");
+        }
+    }
+}
